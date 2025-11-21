301f453d9fa0525d4658d3296de464f4d825ae38
viqueen <hasnae@viqueen.org>
2025-11-21 18:43:35 +1100

    lint fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/types/descriptorpb"
-	"google.golang.org/protobuf/types/pluginpb"
 	"io"
 	"log"
 	"os"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/descriptorpb"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 func main() {
@@ -14,6 +15,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to read from stdin: %v", err)
 	}
+
 	request := &pluginpb.CodeGeneratorRequest{}
 	if err = proto.Unmarshal(data, request); err != nil {
 		log.Fatalf("failed to unmarshal input: %v", err)
@@ -23,13 +25,18 @@ func main() {
 	for _, protoFile := range request.GetProtoFile() {
 		processFile(protoFile, response)
 	}
+
 	respond(response)
 }
 
-func processFile(protoFile *descriptorpb.FileDescriptorProto, response *pluginpb.CodeGeneratorResponse) {
+func processFile(
+	protoFile *descriptorpb.FileDescriptorProto,
+	response *pluginpb.CodeGeneratorResponse,
+) {
 	services := protoFile.GetService()
 	for _, service := range services {
 		log.Printf("service: %v", service)
+
 		methods := service.GetMethod()
 		for _, method := range methods {
 			log.Printf("method: %v", method)
@@ -37,11 +44,12 @@ func processFile(protoFile *descriptorpb.FileDescriptorProto, response *pluginpb
 	}
 }
 
-func respond(resp *pluginpb.CodeGeneratorResponse) {
-	out, err := proto.Marshal(resp)
+func respond(response *pluginpb.CodeGeneratorResponse) {
+	out, err := proto.Marshal(response)
 	if err != nil {
 		log.Fatalf("Failed to marshal response: %v", err)
 	}
+
 	_, err = os.Stdout.Write(out)
 	if err != nil {
 		log.Fatalf("Failed to write response: %v", err)
