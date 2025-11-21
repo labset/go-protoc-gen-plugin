52a88af160bdba1375900a64853c22be5fd1aa9f
viqueen <hasnae@viqueen.org>
2025-11-21 21:47:42 +1100

    use makefile

--- bin/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker run \
-  --volume "${PWD}":/go/src \
-  --workdir /go/src \
-  goreleaser/goreleaser:v2.12.7 \
-  build --clean --snapshot
--- bin/lint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm \
-  --volume "${PWD}":/go/src \
-  --workdir /go/src/ \
-  golangci/golangci-lint:v2.5.0 \
-  golangci-lint run "$@"
