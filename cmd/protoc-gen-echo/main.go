47e920946819a2fb7fc9035434c3c8df62d85e5d
Hasnae <hasnae@labset.org>
2026-05-15 22:13:01 +1000

    prepare plugin shell

---
+++ cmd/protoc-gen-echo/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/labset/go-protoc-gen-plugin/internal/codegen"
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+func main() {
+	protogen.Options{}.Run(func(plugin *protogen.Plugin) error {
+		generator, err := codegen.GeneratorForMode(plugin.Request.GetParameter())
+		if err != nil {
+			return err
+		}
+		return generator.Generate(plugin)
+	})
+}
